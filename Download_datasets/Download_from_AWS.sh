ba7007a19292a8b65e8ef40bcd3292d06d012b8d
Venkatesh <svenkatesh.95@gmail.com>
2021-10-25 23:49:44 +0200

    test

--- Download_datasets/Download_from_AWS.sh
+++
@@ -1,6 +0,0 @@
-# Include the subject in the command line while executing this script -> ex. sh dl.sh subject_list
-for CURSUBJ in $(cat $1)
-do
-    aws s3 cp s3://fcp-indi/data/Projects/HBN/EEG/${CURSUBJ}/ ${CURSUBJ}/ --no-sign-request --recursive
-done
-
